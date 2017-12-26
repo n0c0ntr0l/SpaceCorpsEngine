6b656ee967625a4ac3840ab43524edd2ede8e39e
Jason Osunkoya <>
2017-12-26 12:53:06 +0000

    TargetFinder now can either be assigned a ship or can search for a random ship that doesn't belong to the player

--- src/com/spacecorps/map/Ships/TargetFinder.java
+++ src/com/spacecorps/map/Ships/TargetFinder.java
@@ -1,18 +1,55 @@
 package com.spacecorps.map.Ships;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
 public class TargetFinder {
 
     private final Ship ship;
-    private HashMap<String, Ship>
+    private Ship currentShipTarget;
+    private volatile HashMap<String, Ship> mapOfShipsInCurrentSector;
+    private HashMap<String, Ship> mapOfNonPlayerShipsInSector;
 
-    public TargetFinder(Ship ship) {
+    public TargetFinder(Ship ship, HashMap mapOfShipsInCurrentSector) {
         this.ship = ship;
+        this.mapOfShipsInCurrentSector = mapOfShipsInCurrentSector;
     }
 
-
-    //Checks to see if
+    //
     public boolean setTargetShipByShipID(String targetShipID) {
-
+        currentShipTarget = mapOfShipsInCurrentSector.get(targetShipID);
+        return currentShipTarget != null;
     }
 
+    private void generateMapOfShipsExcludingPlayerOwnedShips() {
+        mapOfNonPlayerShipsInSector = new HashMap();
+        for (Map.Entry<String, Ship> entry : this.mapOfShipsInCurrentSector.entrySet()) {
+            if (!(entry.getValue().getPlayer() == this.ship.getPlayer())) {
+                this.mapOfNonPlayerShipsInSector.put(entry.getKey(), entry.getValue());
+            }
+        }
+    }
+
+    //currently picks a random target
+    public boolean pickRandomTarget() {
+        Random rng = new Random();
+        boolean doesShipBelongToPlayer = false;
+        ArrayList<Ship> listOfNonPlayerShips = generateArrayListOfNonPlayerShips();
+        if (listOfNonPlayerShips.size() < 1) {
+            return false;
+        }
+        int shipToPick = rng.nextInt(listOfNonPlayerShips.size());
+        this.currentShipTarget = listOfNonPlayerShips.get(shipToPick);
+        return true;
+    }
+
+    private ArrayList<Ship> generateArrayListOfNonPlayerShips() {
+        ArrayList<Ship> listOfNonPlayerShips = new ArrayList();
+        for (Map.Entry<String, Ship> entry : this.mapOfNonPlayerShipsInSector.entrySet()) {
+            listOfNonPlayerShips.add(entry.getValue());
+        }
+        return listOfNonPlayerShips;
+    }
 }
