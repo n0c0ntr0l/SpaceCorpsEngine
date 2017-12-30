6366b35afc989cc383a6761d2d0277a88b21d626
Jason Osunkoya <jacemano@gmail.com>
2017-12-30 14:51:19 +0000

    Merge branch 'master' of https://github.com/n0c0ntr0l/SpaceCorpsEngine
                                                                          Bug fix so that ships are now added and removed from sectors correctly

---
+++ src/com/spacecorps/map/Ships/BasicShip.java
@@ -0,0 +1,161 @@
+package com.spacecorps.map.Ships;
+
+import com.spacecorps.map.MainEngine;
+import com.spacecorps.map.Players.Player;
+import com.spacecorps.map.Sector.Sector;
+import com.spacecorps.map.Util;
+import com.spacecorps.map.XYZcoord;
+
+import java.util.Calendar;
+
+public class BasicShip implements Ship {
+    private XYZcoord location;
+    private final String shipID;
+    private final Player shipOwner;
+    private final int shipHullTotal;
+    private final int shipShieldTotal;
+    private int shipHullCurrent;
+    private int shipShieldCurrent;
+    private int shipHullDamagePerSecond;
+    private int shipShieldDamagePerSecond;
+    private double shipMovementSpeed;
+    private boolean isShipTraveling;
+    private XYZcoord shipDestination;
+    private XYZcoord shipOrigin;
+    private Ship targetShip;
+    private boolean isShipDead;
+    private double distanceToTargetTotal;
+    private double distanceToTargetCurrent; //to be used to state how far from sector
+    private Sector currentShipSector;
+    private long endOfJourneyTime;
+    private long startJourneyTime;
+
+    private final MainEngine mainCallBack;
+
+
+    private TargetStatus targetStatus;
+
+    public BasicShip(String shipID, Player shipOwner, int x, int y, int z, int shipHullTotal, int shipShieldTotal, int shipHullDamagePerSecond, int shipShieldDamagePerSecond, double shipMovementSpeed, MainEngine mainCallBack) {
+        this.shipID = shipID;
+        this.shipOwner = shipOwner;
+        this.shipHullTotal = shipHullTotal;
+        this.shipShieldTotal = shipShieldTotal;
+        this.shipHullCurrent = shipHullTotal;
+        this.shipShieldCurrent = shipShieldTotal;
+        this.location = new XYZcoord(x, y, z);
+        this.shipHullDamagePerSecond = shipHullDamagePerSecond;
+        this.shipShieldDamagePerSecond = shipShieldDamagePerSecond;
+        this.isShipDead = false;
+        this.shipMovementSpeed = shipMovementSpeed;
+        this.mainCallBack = mainCallBack;
+        addShipToSector(location);
+    }
+
+    @Override
+    public TargetStatus getTargetStatus() {
+        return targetStatus;
+    }
+
+    @Override
+    public void setTargetStatus(TargetStatus targetStatus) {
+        this.targetStatus = targetStatus;
+    }
+
+    @Override
+    public void takeDamage(int HullDamage, int ShieldDamage) {
+        if (shipShieldCurrent == 0) {
+            shipHullCurrent = -HullDamage;
+        }
+        shipShieldCurrent = -ShieldDamage;
+        if (shipHullCurrent <= 0) {
+            isShipDead = true;
+        }
+    }
+
+    @Override
+    public void setStartJourneyTime() {
+        Calendar cal = Calendar.getInstance();
+        this.startJourneyTime = cal.getTime().getTime();
+    }
+
+    @Override
+    public void removeShipFromSector() {
+        Sector sectorFromLocation = mainCallBack.getSector(location.getxAbsolute(), location.getyAbsolute(), location.getzAbsolute());
+        sectorFromLocation.removeShipFromSector(this);
+    }
+
+    @Override
+    public void setMovingTarget(XYZcoord shipDestination) {
+        this.shipDestination = shipDestination;
+        distanceToTargetTotal = Util.calculateDistanceBetweenTwoPoint(location, shipDestination);
+        mainCallBack.addShipToListOfMovingShips(this);
+    }
+
+    public void addShipToSector(XYZcoord coord) {
+        currentShipSector = mainCallBack.getSector(coord.getxAbsolute(), coord.getyAbsolute(), coord.getzAbsolute());
+        currentShipSector.addShipToSector(this);
+    }
+
+    @Override
+    public void moveShip() {
+        XYZcoord newPosition = Util.getPositionGivenSpeedAndVelocityDirection(this.shipMovementSpeed, this.location, this.shipDestination);
+        double distanceToTargetCurrent;
+        if (!this.isShipTraveling) {
+            this.isShipTraveling = true;
+            setStartJourneyTime();
+            distanceToTargetTotal = Util.calculateDistanceBetweenTwoPoint(location, shipDestination);
+            this.distanceToTargetCurrent = distanceToTargetTotal;
+        } else {
+            distanceToTargetCurrent = Util.calculateDistanceBetweenTwoPoint(location, shipDestination);
+        }
+        if (newPosition.equals(this.shipDestination)) {
+            this.isShipTraveling = false;
+            this.shipOrigin = newPosition;
+            mainCallBack.removeShipFromListOfMovingShip(this);
+            setEndOfJourneyTime();
+            if (MainEngine.LOGLEVEL > 0) {
+                System.out.println(this.getShipID() + " has arrived at destination" + newPosition.xAbsolute + "," + newPosition.getyAbsolute() + "," + newPosition.getzAbsolute());
+                System.out.println(this.getShipID() + " took " + numberOfSecondsTravelled() + " seconds to reach the destination");
+            }
+        }
+        if (!newPosition.equals(location)) {
+            if (MainEngine.LOGLEVEL > 0) {
+                //System.out.println(this.getShipID() + " has moved from sector " + location.getxAbsolute() + "," + location.getyAbsolute() + "," + location.getzAbsolute() +
+                //        " to " + newPosition.getxAbsolute() + "," + newPosition.getyAbsolute() + "," + newPosition.getzAbsolute());
+                //System.out.println(this.getShipID() + " has moved from sector " + location.toString() + " to " + newPosition.toString());
+            }
+            this.currentShipSector.removeShipFromSector(this);
+            this.addShipToSector(newPosition);
+        }
+        if (this.currentShipSector.getNumOfShipsInSector() > 1) {
+            this.mainCallBack.addSectorToHotSectors(this.currentShipSector);
+        }
+        this.location = newPosition;
+    }
+
+    @Override
+    public void setEndOfJourneyTime() {
+        Calendar cal = Calendar.getInstance();
+        this.endOfJourneyTime = cal.getTimeInMillis();
+    }
+
+    @Override
+    public int numberOfSecondsTravelled() {
+        return (int) ((this.endOfJourneyTime - this.startJourneyTime) / 1000);
+    }
+
+    @Override
+    public boolean isShipTravelling() {
+        return isShipTraveling;
+    }
+
+    @Override
+    public String getShipID() {
+        return this.shipID;
+    }
+
+    @Override
+    public XYZcoord getLocation() {
+        return location;
+    }
+}
--- src/com/spacecorps/map/Ships/Ship.java
+++ src/com/spacecorps/map/Ships/Ship.java
@@ -1,152 +1,34 @@
 package com.spacecorps.map.Ships;
 
-import com.spacecorps.map.MainEngine;
-import com.spacecorps.map.Players.Player;
 import com.spacecorps.map.Sector.Sector;
-import com.spacecorps.map.Util;
 import com.spacecorps.map.XYZcoord;
 
-import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Date;
 
-public class Ship {
-    private XYZcoord location;
-    private final String shipID;
-    private final Player shipOwner;
-    private final int shipHullTotal;
-    private final int shipShieldTotal;
-    private int shipHullCurrent;
-    private int shipShieldCurrent;
-    private int shipHullDamagePerSecond;
-    private int shipShieldDamagePerSecond;
-    private double shipMovementSpeed;
-    private boolean isShipTraveling;
-    private XYZcoord shipDestination;
-    private XYZcoord shipOrigin;
-    private Ship targetShip;
-    private boolean isShipDead;
-    private double distanceToTargetTotal;
-    private double distanceToTargetCurrent; //to be used to state how far from sector
-    private Sector currentShipSector;
-    private long endOfJourneyTime;
-    private long startJourneyTime;
+public interface Ship {
+    void addShipToSector(XYZcoord coord);
 
-    private final MainEngine mainCallBack;
+    void removeShipFromSector();
 
+    TargetStatus getTargetStatus();
 
+    void setTargetStatus(TargetStatus targetStatus);
 
-    private TargetStatus targetStatus;
+    void takeDamage(int HullDamage, int ShieldDamage);
 
-    public Ship(String shipID, Player shipOwner, int x, int y, int z, int shipHullTotal, int shipShieldTotal, int shipHullDamagePerSecond, int shipShieldDamagePerSecond, double shipMovementSpeed, MainEngine mainCallBack) {
-        this.shipID = shipID;
-        this.shipOwner = shipOwner;
-        this.shipHullTotal = shipHullTotal;
-        this.shipShieldTotal = shipShieldTotal;
-        this.shipHullCurrent = shipHullTotal;
-        this.shipShieldCurrent = shipShieldTotal;
-        this.location = new XYZcoord(x, y, z);
-        this.shipHullDamagePerSecond = shipHullDamagePerSecond;
-        this.shipShieldDamagePerSecond = shipShieldDamagePerSecond;
-        this.isShipDead = false;
-        this.shipMovementSpeed = shipMovementSpeed;
-        this.mainCallBack = mainCallBack;
-        addShipToSector(location);
-    }
+    void setMovingTarget(XYZcoord shipDestination);
 
-    private void addShipToSector(XYZcoord coord) {
-        currentShipSector = mainCallBack.getSector(coord.getxAbsolute(), coord.getyAbsolute(), coord.getzAbsolute());
-        currentShipSector.addShipToSector(this);
-    }
+    void moveShip();
 
-    private void removeShipFromSector() {
-        Sector sectorFromLocation = mainCallBack.getSector(location.getxAbsolute(), location.getyAbsolute(), location.getzAbsolute());
-        sectorFromLocation.removeShipFromSector(this);
-    }
+    void setStartJourneyTime();
 
-    public TargetStatus getTargetStatus() {
-        return targetStatus;
-    }
+    void setEndOfJourneyTime();
 
-    public void setTargetStatus(TargetStatus targetStatus) {
-        this.targetStatus = targetStatus;
-    }
+    int numberOfSecondsTravelled();
 
-    public void takeDamage(int HullDamage, int ShieldDamage){
-        if(shipShieldCurrent == 0){
-            shipHullCurrent =- HullDamage;
-        }
-        shipShieldCurrent =- ShieldDamage;
-        if(shipHullCurrent <= 0){
-            isShipDead = true;
-        }
-    }
+    boolean isShipTravelling();
 
-    public void setMovingTarget(XYZcoord shipDestination){
-        this.shipDestination = shipDestination;
-        distanceToTargetTotal = Util.calculateDistanceBetweenTwoPoint(location,shipDestination);
-        mainCallBack.addShipToListOfMovingShips(this);
-    }
+    String getShipID();
 
-    public void moveShip(){
-        XYZcoord newPosition = Util.getPositionGivenSpeedAndVelocityDirection(this.shipMovementSpeed,this.location,this.shipDestination);
-        double distanceToTargetCurrent;
-        if(!this.isShipTraveling){
-        	this.isShipTraveling = true;
-            setStartJourneyTime();
-            distanceToTargetTotal = Util.calculateDistanceBetweenTwoPoint(location, shipDestination);
-        	this.distanceToTargetCurrent = distanceToTargetTotal;
-        } else {
-        	distanceToTargetCurrent = Util.calculateDistanceBetweenTwoPoint(location, shipDestination);
-        }
-        if(newPosition.equals(this.shipDestination)){
-        	this.isShipTraveling = false;
-        	this.shipOrigin = newPosition;
-            mainCallBack.removeShipFromListOfMovingShip(this);
-            setEndOfJourneyTime();
-            if (MainEngine.LOGLEVEL > 0) {
-                System.out.println(this.getShipID() + " has arrived at destination" + newPosition.xAbsolute + "," + newPosition.getyAbsolute() + "," + newPosition.getzAbsolute());
-                System.out.println(this.getShipID() + " took " + numberOfSecondsTravelled() + " seconds to reach the destination");
-            }
-        }
-        if (!newPosition.equals(location)) {
-            if (MainEngine.LOGLEVEL > 0) {
-                //System.out.println(this.getShipID() + " has moved from sector " + location.getxAbsolute() + "," + location.getyAbsolute() + "," + location.getzAbsolute() +
-                //        " to " + newPosition.getxAbsolute() + "," + newPosition.getyAbsolute() + "," + newPosition.getzAbsolute());
-                //System.out.println(this.getShipID() + " has moved from sector " + location.toString() + " to " + newPosition.toString());
-            }
-            this.currentShipSector.removeShipFromSector(this);
-            this.addShipToSector(newPosition);
-        }
-        if (this.currentShipSector.getNumOfShipsInSector() > 1) {
-            this.mainCallBack.addSectorToHotSectors(this.currentShipSector);
-        }
-        this.location = newPosition;
-    }
-
-    private void setStartJourneyTime() {
-        Calendar cal = Calendar.getInstance();
-        this.startJourneyTime = cal.getTime().getTime();
-    }
-
-    private void setEndOfJourneyTime() {
-        Calendar cal = Calendar.getInstance();
-        this.endOfJourneyTime = cal.getTimeInMillis();
-    }
-
-    private int numberOfSecondsTravelled() {
-        return (int) ((this.endOfJourneyTime - this.startJourneyTime) / 1000);
-    }
-    
-    public boolean isShipTravelling(){
-    	return isShipTraveling;
-    }
-
-    public String getShipID() {
-        return this.shipID;
-    }
-
-    public XYZcoord getLocation() {
-        return location;
-    }
+    XYZcoord getLocation();
 }
